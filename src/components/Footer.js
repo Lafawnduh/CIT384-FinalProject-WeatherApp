43177fdb6bf52ec076a0e17795029e59d6357f8d
DanielJ312 <danieljavaherian13@gmail.com>
2023-05-14 22:39:24 -0700

    Updated Footer to include source code3

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,10 @@ import '../styles/Footer.css';
 function Footer() {
   return (
     <footer className="footer">
-      <p className="footer-text">&copy; Matador Weather {new Date().getFullYear()}</p>
+      <p className="footer-text">
+        &copy; Matador Weather {new Date().getFullYear()}<br></br>
+        <a href='https://github.com/Lafawnduh/CIT384-FinalProject-WeatherApp' title="GitHub Repositiory">Source Code</a>
+      </p>
     </footer>
   );
 }
